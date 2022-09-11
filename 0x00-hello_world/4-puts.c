35829ed676bb509c8b6b0b7c86010ff7ea9af77c
Miyebianny <awulehananita@gmail.com>
2022-09-11 07:14:55 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+^C
+root@f2e2d1410582:/alx-low_level_programming/0x00-hello_world# chmod u+x 4-puts.c
+
+^c
+cd ..
+^C
+root@f2e2d1410582:/alx-low_level_programming/0x00-hello_world#
