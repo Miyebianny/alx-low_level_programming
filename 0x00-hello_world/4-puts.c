fc8cfebb0fa34a8bc5def42b3f9dd7500687f6f3
Miyebianny <awulehananita@gmail.com>
2022-09-12 08:12:04 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,11 +10,3 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
-^C
-root@f2e2d1410582:/alx-low_level_programming/0x00-hello_world# chmod u+x 4-puts.c
-
-^c
-cd ..
-^C
-root@f2e2d1410582:/alx-low_level_programming/0x00-hello_world#
