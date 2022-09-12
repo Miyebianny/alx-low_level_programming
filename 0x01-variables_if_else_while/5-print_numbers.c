aaf4910ede14cbe539f08337a2ad1103758d0449
Miyebianny <awulehananita@gmail.com>
2022-09-12 12:23:18 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main ->Entry point
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
+
