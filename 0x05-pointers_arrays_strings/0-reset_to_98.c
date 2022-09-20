79f65e4a56cceebdc6b41edd8989d7dd69cae261
Miyebianny <awulehananita@gmail.com>
2022-09-20 06:29:57 -0700

    Write a function that takes a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-*reset_to_98 -resets the value of given integer to 98
-*@n: parameter
-*Return : nothing
-*/
-
+ * reset_to_98 -> Write a function to update the reference of a pointer
+ * @n: pointer n to be updated
+ */
 void reset_to_98(int *n)
 {
-	*n - 98;
+	*n = 98;
 }
 
