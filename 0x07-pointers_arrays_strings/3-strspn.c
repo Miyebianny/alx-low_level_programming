9bed987b832c6d7ebf3406fa3109c720fdc2aa2c
Miyebianny <awulehananita@gmail.com>
2022-09-27 10:07:20 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input string
+ * @accept: accepted string
+ *
+ * Return: Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (t);
+}
+
